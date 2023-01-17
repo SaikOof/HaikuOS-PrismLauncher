5fea74b95654e6237a30aa8b1d7df79a1914e29b
SaikOof <102391432+SaikOof@users.noreply.github.com>
2023-01-17 19:10:49 +0100

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -3,7 +3,7 @@ export DEPS="cmake coreutils gcc git openjdk14_default qt5_devel sed"
 pkgman install $DEPS
 
 git clone --recursive https://github.com/PrismLauncher/PrismLauncher src
-mkdir "install"
+mkdir -p "install/jars"
 
 #START patching files
 sed -i '0,/.*include(KDEInstallDirs).*/s/.*include(KDEInstallDirs).*/set(CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}\/libraries\/extra-cmake-modules\/kde-modules;${CMAKE_MODULE_PATH}")\n&/' src/CMakeLists.txt
@@ -19,3 +19,9 @@ cmake -S src -B src/build \
 #Not using one thread causes the build to get stuck at 99%
 cmake --build src/build -j1
 cmake --install src/build
+
+mv install/bin/launcher install
+mv install/share/prismlauncher/* install/jars
+
+rmdir bin
+rm -rf share
