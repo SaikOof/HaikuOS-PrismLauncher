75d321d937fc4b498d8e82811d28dce2b44f0e29
SaikOof <102391432+SaikOof@users.noreply.github.com>
2023-01-17 19:26:14 +0100

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
-export DEPS="cmake coreutils gcc git openjdk14_default qt5_devel sed"
+#!/bin/bash
+export DEPS="bash cmake coreutils gcc git openjdk14_default qt5_devel sed"
 pkgman install $DEPS
 
 rm src/CMakeLists.txt
 rm src/launcher/UpdateController.cpp
 
-git clone --recurse-submodules src
+cd src && git pull --recurse-submodules && cd -
 
 #START patching files
 sed -i '0,/.*include(KDEInstallDirs).*/s/.*include(KDEInstallDirs).*/set(CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}\/libraries\/extra-cmake-modules\/kde-modules;${CMAKE_MODULE_PATH}")\n&/' src/CMakeLists.txt
