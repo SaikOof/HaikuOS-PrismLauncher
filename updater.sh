05d0deba109d782522abf59291d954a5248fa3bb
SaikOof <102391432+SaikOof@users.noreply.github.com>
2023-01-17 19:06:01 +0100

    Create updater.sh

---
+++ updater.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+export DEPS="cmake coreutils gcc git openjdk14_default qt5_devel sed"
+pkgman install $DEPS
+
+rm src/CMakeLists.txt
+rm src/launcher/UpdateController.cpp
+
+git clone --recurse-submodules src
+
+#START patching files
+sed -i '0,/.*include(KDEInstallDirs).*/s/.*include(KDEInstallDirs).*/set(CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}\/libraries\/extra-cmake-modules\/kde-modules;${CMAKE_MODULE_PATH}")\n&/' src/CMakeLists.txt
+sed -i 's/#error Unsupported operating system./QString finishCmd = FS::PathCombine(m_root, BuildConfig.LAUNCHER_NAME);/' src/launcher/UpdateController.cpp
+#END patching files
+
+cmake -S src -B src/build \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX="install" \
+      -DENABLE_LTO=ON \
+      -DECM_MODULE_DIR="libraries/extra-cmake-modules/modules" \
+
+#Not using one thread causes the build to get stuck at 99%
+cmake --build src/build -j1
+cmake --install src/build
