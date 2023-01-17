e85de29042775aa32fcb6624d89a9c15e3e65626
SaikOof <102391432+SaikOof@users.noreply.github.com>
2023-01-17 18:57:22 +0100

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-#INSERT THIS SCRIPT INSIDE A FOLDER BEFORE RUNNING
-
 export DEPS="cmake coreutils gcc git openjdk14_default qt5_devel sed"
 pkgman install $DEPS
 
+git clone --recursive https://github.com/PrismLauncher/PrismLauncher src
 mkdir "install"
+
 #START patching files
 sed -i '0,/.*include(KDEInstallDirs).*/s/.*include(KDEInstallDirs).*/set(CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}\/libraries\/extra-cmake-modules\/kde-modules;${CMAKE_MODULE_PATH}")\n&/' src/CMakeLists.txt
-#sed ''
+sed -i 's/#error Unsupported operating system./QString finishCmd = FS::PathCombine(m_root, BuildConfig.LAUNCHER_NAME);/' src/launcher/UpdateController.cpp
 #END patching files
 
 cmake -S src -B src/build \
@@ -16,5 +16,6 @@ cmake -S src -B src/build \
       -DENABLE_LTO=ON \
       -DECM_MODULE_DIR="libraries/extra-cmake-modules/modules" \
 
+#Not using one thread causes the build to get stuck at 99%
 cmake --build src/build -j1
 cmake --install src/build
