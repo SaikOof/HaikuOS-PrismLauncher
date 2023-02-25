af2f036b20754049d086f40117bf02015b592cce
SaikOof <102391432+SaikOof@users.noreply.github.com>
2023-02-25 20:56:10 +0100

    fixed for the remove of UpdateController.cpp

--- builder.sh
+++ builder.sh
@@ -7,7 +7,6 @@ mkdir -p "install/jars"
 
 #START patching files
 sed -i '0,/.*include(KDEInstallDirs).*/s/.*include(KDEInstallDirs).*/set(CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}\/libraries\/extra-cmake-modules\/kde-modules;${CMAKE_MODULE_PATH}")\n&/' src/CMakeLists.txt
-sed -i 's/#error Unsupported operating system./QString finishCmd = FS::PathCombine(m_root, BuildConfig.LAUNCHER_NAME);/' src/launcher/UpdateController.cpp
 #END patching files
 
 cmake -S src -B src/build \
